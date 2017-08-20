30cdde5157def9513d50963fb415c1b82181cc37
Mauricio Laine <laine.mau@gmail.com>
2017-08-20 19:22:58 -0400

    receive data from the api and print the data set with logd

--- app/src/main/java/com/example/mauro/daggerplus/ui/main/MainActivity.java
+++ app/src/main/java/com/example/mauro/daggerplus/ui/main/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.mauro.daggerplus.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.mauro.daggerplus.R;
+import com.example.mauro.daggerplus.data.entities.Result;
+import com.example.mauro.daggerplus.data.entities.ResultApi;
+import com.example.mauro.daggerplus.data.remote.MovieService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doMagic(View view) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MovieService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        MovieService movieService = retrofit.create(MovieService.class);
+        movieService.getMovies(MovieService.API_KEY).enqueue(new Callback<ResultApi>() {
+            @Override
+            public void onResponse(Call<ResultApi> call, Response<ResultApi> response) {
+                ResultApi resultApi = response.body();
+                for (Result result: resultApi.getResults()){
+                    Log.d(TAG, "onResponse: " +  result.getTitle());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResultApi> call, Throwable t) {
+
+            }
+        });
     }
 }
